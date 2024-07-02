d587699900b98cb7c34edcbc6a5c90d825b782f5
Ito <waskitodamar17@gmail.com>
2024-07-02 10:21:23 +0700

    - logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,19 +22,19 @@
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-light">
 
     <div class="container">
 
         <!-- Outer Row -->
         <div class="row justify-content-center align-items-center">
             <div class="col-xl-10 col-lg-8 col-md-9 d-flex justify-content-center align-items-center">
-                <div class="card o-hidden border-0 shadow-lg my-5 col-9">
+                <div class="card o-hidden border-0 shadow-lg my-5 col-9 bg-gradient-danger">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
+                            <div class="text-center mb-5">
+                                <img src="{{ asset('img/logo_miokard_login-01.png') }}" alt="">
                             </div>
                             @if (session()->has('error'))
                                 <div class="alert alert-danger" role="alert">
@@ -51,7 +51,7 @@
                                     <input type="password" name="password" class="form-control form-control-user"
                                         id="exampleInputPassword" placeholder="Password">
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                <button type="submit" class="btn btn-secondary btn-user btn-block">Login</button>
                             </form>
                             <hr>
                             {{-- <div class="text-center">
--- resources/views/components/teacher/sidebar.blade.php
+++ resources/views/components/teacher/sidebar.blade.php
@@ -1,13 +1,10 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
         <div class="sidebar-brand-text mx-3">
-            Miokard <sup>2</sup>
+            <img src="{{ asset('img/logomiokard.png') }}" alt="">
         </div>
     </a>
 
